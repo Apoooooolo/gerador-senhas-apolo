5703e45d3e2eaedeadae625e0cf60a5cb61d8c53
Apoooooolo <160729976+Apoooooolo@users.noreply.github.com>
2025-08-12 08:50:39 -0300

    ghh

---
+++ main.js
@@ -0,0 +1,15 @@
+const numeroSenha = document.querySelector('.paramero-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.paramero-senha__botao')
+
+botoes[0].onclick = diminuiTamanho;
+botoes[0].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    if(tamanhoSenha > 1){
+        tamanhoSenha = tamanhoSenha - 1;
+    }
+    numeroSenha.textContent=tamanhoSenha;
+}
