f03ba535b2d46584d23a22e603c45b6e68e99c56
Apoooooolo <160729976+Apoooooolo@users.noreply.github.com>
2025-08-26 07:43:52 -0300

    fxb

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const numeroSenha = document.querySelector('.paramero-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
-const botoes = document.querySelectorAll('.paramero-senha__botao')
+const botoes = document.querySelectorAll('.paramero-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
